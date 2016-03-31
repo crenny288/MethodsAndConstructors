500fe157e71261d7e0ad70f7c9a53568289e2815
crenny288 <crenny288@gmail.com>
2016-03-31 15:43:43 -0400

    Adding Person, car, and Building classes

---
+++ src/com/theironyard/clt/Building.java
@@ -0,0 +1,7 @@
+package com.theironyard.clt;
+
+/**
+ * Created by mac on 3/31/16.
+ */
+public class Building {
+}
---
+++ src/com/theironyard/clt/Person.java
@@ -0,0 +1,21 @@
+package com.theironyard.clt;
+
+/**
+ * Created by mac on 3/31/16.
+ */
+public class Person {
+    private String name;
+    private String hairColor;
+    private int age:
+
+    Person(String name, String hairColor, int age) {
+        this.name = name;
+        this.hairColor = hairColor;
+        this.age = age;
+    }
+
+    public void setAge(age) {
+
+
+}
+
