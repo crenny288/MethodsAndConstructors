8a4357ec0aa3757a845a910bc70ce0dc3e9e80d1
crenny288 <crenny288@gmail.com>
2016-03-31 17:01:15 -0400

    Adding Person, car and Building classes

--- src/com/theironyard/clt/Car.java
+++ src/com/theironyard/clt/Car.java
@@ -4,4 +4,17 @@ package com.theironyard.clt;
  * Created by mac on 3/31/16.
  */
 public class Car {
+    String model;
+    String color;
+    String year;
+
+
+    Car(String model, String color, String year) {
+        this.model = model;
+        this.color = color;
+        this.year = year;
+    }
+
+
+
 }
