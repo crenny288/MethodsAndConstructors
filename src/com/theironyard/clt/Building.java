ea292a108ad43fafa9b460ec2dafd6f14f57a597
crenny288 <crenny288@gmail.com>
2016-04-01 15:48:43 -0400

    Adding Person, car and Building classes

--- src/com/theironyard/clt/Building.java
+++ src/com/theironyard/clt/Building.java
@@ -4,4 +4,44 @@ package com.theironyard.clt;
  * Created by mac on 3/31/16.
  */
 public class Building {
+    private String type;
+    private String style;
+    private String community;
+
+    Building(String type, String style, String community) {
+        this.type = type;
+        this.style = style;
+        this.community = community;
+
+    }
+
+    public void setType(type) {
+        this.type = type;
+
+    }
+
+    public String getType() {
+        return this.type;
+
+    }
+
+    public void setStyle(style) {
+        this.style = style;
+
+    }
+
+    public String getStyle() {
+        return this.style;
+
+    }
+
+    public void setCommunity(community) {
+        this.community = community;
+        
+    }
+
+    public String getCommunity() {
+        return this.community;
+
+    }
 }
